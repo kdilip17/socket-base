4e6d6a139da8d8f68bfa844b14ba14e546b189fe
dilip <kdilip17@gmail.com>
2022-11-19 21:49:27 +0530

    switch case added

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const wss = new WebSocket.Server({ server: server });
 wss.on("connection", (ws) => {
   ws.send("Welcome to the chat :)");
 
+  setInterval(() => {
+    ws.send(
+      JSON.stringify({
+        type: "HEARTBEAT",
+        status: "Healthy",
+        updatedAt: new Date(),
+      })
+    );
+  }, 10000);
+
   ws.on("message", (data) => {
     let message;
     let actionTime = new Date();
@@ -18,51 +28,57 @@ wss.on("connection", (ws) => {
       sendError(ws, "Bad formatted payload, non JSON", actionTime);
       return;
     }
-    console.log(`Message Type: ${message.type.toLowerCase()}`)
-    if (message.type.toLowerCase() === "subscribe") {
-      wss.clients.forEach(function each(client) {
-        actionTime = new Date();
-        setTimeout(() => {
-          client.send(
-            JSON.stringify({
-              type: message.type,
-              status: "Subscribed",
-              updatedAt: actionTime,
-              // size: wss.clients.size,
-            })
-          );
-        }, 0);
-      });
-    } else if (message.type.toLowerCase() === "unsubscribe") {
-      wss.clients.forEach(function each(client) {
-        if (client === ws && client.readyState === WebSocket.OPEN) {
+    console.log(`Message Type: ${message.type.toLowerCase()}`);
+    switch (message.type.toLowerCase()) {
+      case "subscribe": {
+        wss.clients.forEach(function each(client) {
           actionTime = new Date();
           setTimeout(() => {
             client.send(
               JSON.stringify({
                 type: message.type,
-                status: "Unsubscribed",
+                status: "Subscribed",
                 updatedAt: actionTime,
-                // size: wss.clients.size,
               })
             );
-            client.close();
           }, 0);
-        }
-      });
-    } else if (message.type.toLowerCase() === "countsubscribers") {
-      actionTime = new Date();
-      ws.send(
-        JSON.stringify({
-          type: message.type,
-          count: wss.clients.size,
-          updatedAt: actionTime,
-        })
-      );
-    } else {
-      wss.clients.forEach(function each(client) {
-        sendError(client, "Requested Method Not Implemented", actionTime);
-      });
+        });
+        break;
+      }
+      case "unsubscribe": {
+        wss.clients.forEach(function each(client) {
+          if (client === ws && client.readyState === WebSocket.OPEN) {
+            actionTime = new Date();
+            setTimeout(() => {
+              client.send(
+                JSON.stringify({
+                  type: message.type,
+                  status: "Unsubscribed",
+                  updatedAt: actionTime,
+                })
+              );
+              client.close();
+            }, 0);
+          }
+        });
+        break;
+      }
+      case "countsubscribers": {
+        actionTime = new Date();
+        ws.send(
+          JSON.stringify({
+            type: message.type,
+            count: wss.clients.size,
+            updatedAt: actionTime,
+          })
+        );
+        break;
+      }
+      default: {
+        wss.clients.forEach(function each(client) {
+          sendError(client, "Requested Method Not Implemented", actionTime);
+        });
+      }
     }
   });
 });
